19be41fb52252118c840982bcfed22e9b7f998c1
Sushil Dikondwar <sushildikondwar@gmail.com>
2022-01-28 12:32:09 +0530

    search for a permutation in a string (v1: multiple count2 array creation)

---
+++ Strings/PermutationInString.cpp
@@ -0,0 +1,40 @@
+
+#include <iostream>
+using namespace std;
+
+bool checkEquality(int count1[26], int count2[26]) {    /* checking char count equality in both arrays */
+    for (int i=0; i<26; i++) {
+        if (count1[i] != count2[i])
+            return false;
+    }
+    return true;
+}
+
+int main() {
+    string s1, s2;
+    int count1[26] = {0};
+    
+    s1 = "ab";
+    s2 = "acaacbooo";
+
+    for (int i=0; i<s1.length(); i++)   /* counting the characters in search string */
+        count1[s1[i]-'a']++;
+    
+    int i=0;
+
+    while (i+s1.length() <= s2.length()) {   /* Search Window */
+        int count2[26] = {0};
+        for (int j=i; j<i+s1.length(); j++) {
+            count2[s2[j]-'a']++;
+            cout << s2[j];
+        }
+        if (checkEquality(count1, count2)) {
+            cout << "  <--  Permutation found!";
+            return 0;
+        }
+        cout << endl;
+        i++;                                /* Advancing the Window */
+    }
+    cout << endl << "Permutation not found!";
+    return 0;
+}
