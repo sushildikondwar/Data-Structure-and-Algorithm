ac17d7e4205b95168400031c95f51588f926273f
Sushil Dikondwar <sushildikondwar@gmail.com>
2022-01-28 12:57:36 +0530

    search for a permutation in a string (v2: reusing count2 array)

--- Strings/PermutationInString.cpp
+++ Strings/PermutationInString.cpp
@@ -13,9 +13,10 @@ bool checkEquality(int count1[26], int count2[26]) {    /* checking char count e
 int main() {
     string s1, s2;
     int count1[26] = {0};
+    int count2[26] = {0};
     
-    s1 = "ab";
-    s2 = "acaacbooo";
+    s1 = "abac";
+    s2 = "acaafabcagsgbgxbgbgxxcbooo";
 
     for (int i=0; i<s1.length(); i++)   /* counting the characters in search string */
         count1[s1[i]-'a']++;
@@ -23,8 +24,9 @@ int main() {
     int i=0;
 
     while (i+s1.length() <= s2.length()) {   /* Search Window */
-        int count2[26] = {0};
-        for (int j=i; j<i+s1.length(); j++) {
+        int j;
+        for (j=i; j<i+s1.length(); j++) {
+            int k=i;
             count2[s2[j]-'a']++;
             cout << s2[j];
         }
@@ -33,6 +35,9 @@ int main() {
             return 0;
         }
         cout << endl;
+        while ( --j >= i ) {
+            count2[s2[j]-'a']--;
+        }
         i++;                                /* Advancing the Window */
     }
     cout << endl << "Permutation not found!";
