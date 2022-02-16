3f214af9c32a61c91d65b757bd7050535537db88
Sushil Dikondwar <sushildikondwar@gmail.com>
2022-02-16 21:18:30 +0530

    Static and Dynamic Memory Allocation (with Dynamic Arrays Implementation)

--- Math/StaticDynamicMemoryAlloc.cpp
+++ Math/StaticDynamicMemoryAlloc.cpp
@@ -22,6 +22,7 @@ int main() {
 
 	// GOOD PRACTICE: Either provide the size of array to be used in program implicitly before compilation, so that the compiler will reserve the large space of Stack Memory while compilation,
 	//OR,
+    //////////////////////DYNAMIC 1D///////////////////////////////////////////////////////
 	int m;
 	cin >> m;
 	int *arr1 = new int[m];	//	Allocate the memory dynamically from the Heap Memory (large bydefault) in runtime using 'new' keyword
@@ -37,6 +38,33 @@ int main() {
 	cout << "Multiple: " << getMultiple(arr1, m) << endl;
 
     delete []arr1; // deletes the space allocated on this pointer
+
+    //////////////////////DYNAMIC 2D///////////////////////////////////////////////////////
+
+	/*	for making 1D array, ( int *arr = new int[n] ), i.e., array is denoted by (int*)	*/
+
+	/*	for making 2D array, we'll make 'm' spaces to store pointers to 'm', (int **arr = new int*[m])
+		we created 'm' spaces to store the starting pointers of arrays, now we need to allocate array starting addresses,
+		simply iterate over a loop, and form new array of size 'n' each time, and store its address to the iterating index	*/
+
+	int row=3, col=3;
+	int **matrix = new int*[row];
+	for (int i=0; i<row; i++) {
+		matrix[i] = new int[col];
+	}
+
+	//	fill matrix
+	for (int i=0; i<row; i++)
+		for (int j=0; j<col; j++)
+			cin >> matrix[i][j];
+
+	//	print matrix
+	cout << endl << "Matrix: " << endl;
+	for (int i=0; i<row; i++) {
+		for (int j=0; j<col; j++)
+			cout << matrix[i][j] << " ";
+		cout << endl;
+	}
 
 	return 0;
 }
