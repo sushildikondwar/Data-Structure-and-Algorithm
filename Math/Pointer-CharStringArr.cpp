71f9fc263f1efb44af5cc88c8ee26537336c79ed
Sushil Dikondwar <sushildikondwar@gmail.com>
2022-02-16 16:14:26 +0530

    Updated Pointer-CharStringArray

--- Math/Pointer-CharStringArr.cpp
+++ Math/Pointer-CharStringArr.cpp
@@ -7,6 +7,20 @@ int main() {
 	cout << *arr+1 << endl;
 	cout << *(arr+2) << endl;
 
+
+	int *arrptr = arr;
+	cout << arrptr << endl;
+
+	cout << arrptr++ << endl;	//	here, the pointer is getting traversed over the array
+	cout << arrptr++ << endl;
+
+	// BUT, IF WE TRY TO INCREMENT THE ARRAY POINTER ITSELF,
+	// cout << arr++ << endl;	// it'll throw an error
+
+	if (arr[2] == 2[arr]) {
+		cout << "Both the statements are equal and legit!!" << endl;
+	}
+
 	char ch[] = "sushildikondwar";
 	cout << ch << endl;
 	cout << sizeof(ch) << endl;
@@ -25,6 +39,8 @@ int main() {
 	char cha = 's';	//	even there's a single character
 	char *ptr = &cha;	//	the reference pointer will treat it as string and will print the characters till it not find null pointer
 	cout << ptr << endl;	//	and in this case, it eventually prints some garbage value
+
+	////////////////////////////////////////////////
 
 	return 0;
 }
